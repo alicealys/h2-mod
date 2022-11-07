5db339850a701154aa8e27c0a3b086148030ee5f
FutureRave <edoardo.sanguineti222@gmail.com>
2022-11-07 10:11:09 +0000

    remove protoc

--- premake5.lua
+++ premake5.lua
@@ -323,13 +323,3 @@ resincludedirs {"$(ProjectDir)src"}
 
 group "Dependencies"
 dependencies.projects()
-
-rule "ProtobufCompiler"
-display "Protobuf compiler"
-location "./build"
-fileExtension ".proto"
-buildmessage "Compiling %(Identity) with protoc..."
-buildcommands {'@echo off', 'path "$(SolutionDir)\\..\\tools"',
-			   'if not exist "$(ProjectDir)\\src\\proto" mkdir "$(ProjectDir)\\src\\proto"',
-			   'protoc --error_format=msvs -I=%(RelativeDir) --cpp_out=src\\proto %(Identity)'}
-buildoutputs {'$(ProjectDir)\\src\\proto\\%(Filename).pb.cc', '$(ProjectDir)\\src\\proto\\%(Filename).pb.h'}
